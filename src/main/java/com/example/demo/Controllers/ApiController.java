3d6540f3043a2fd047c45dfe1ad0ce6042dcd64b
indhuramesh <indhu131003@gmail.com>
2023-02-22 12:19:21 +0530

    first commit

---
+++ src/main/java/com/example/demo/Controllers/ApiController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controllers;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class ApiController {
+	@Value("${value}")
+	private String name;
+	
+	@GetMapping("/")
+	public String display() {
+		return "Welcometo the session "+name;
+	}
+	
+}
---
+++ src/main/java/com/example/demo/Day2ValueApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2ValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2ValueApplication.class, args);
+	}
+
+}
